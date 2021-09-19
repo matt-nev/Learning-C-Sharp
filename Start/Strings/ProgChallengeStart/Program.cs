2f5f9c65760ccd2fa0962769dbde75725a7977ba
Matt Nev <mattnev@microsoft.com>
2021-09-19 13:19:01 -0500

    Add strings finished code

--- Start/Strings/ProgChallengeStart/Program.cs
+++ Start/Strings/ProgChallengeStart/Program.cs
@@ -8,6 +8,7 @@ namespace ProgChallengeStart
         {
             // Choose a random number between 0 and 20
             int theNumber = new Random().Next(20);
+            bool keepGoing = true;
 
             // Print the game greeting and instructions
             Console.WriteLine("Let's Play 'Guess the Number'!");
@@ -15,10 +16,52 @@ namespace ProgChallengeStart
             Console.WriteLine("Enter your guess, or -1 to give up.");
 
             // Keep track of the number of guesses and the current user guess
+            int guessNum = 0;
+            int guessCount = 0;
 
-            // Start the game and run until user quits or guesses correctly
-            // HINT: You'll need a way to convert the user's input to an integer
-            
+            // Use a do-while loop becaue we know we want it to execute at least once 
+            do
+            {
+                // Ask user for current guess
+                Console.WriteLine("What is your guess?");
+                string theGuess = Console.ReadLine();
+
+                // Use TryParse method to parse the input into a number
+                bool result = Int32.TryParse(theGuess, out guessNum);
+
+                // If the user entered something other than a number ask them again
+                if (!result)
+                {
+                    Console.WriteLine("Hmm, that doesn't look like a number. Try again.");
+                }
+                else
+                {
+                    // If they enter a -1 then exit the game
+                    if (guessNum == -1)
+                    {
+                        Console.WriteLine($"Oh well. I was thinking of {theNumber}");
+                        keepGoing = false;
+                    }
+                    else
+                    {
+                        // Increase guess count 
+                        guessCount++;
+
+                        // If they got it right, tell them the guess count and exit
+                        if (guessNum == theNumber)
+                        {
+                            Console.WriteLine($"You got it in {guessCount} guesses!");
+                            keepGoing = false;
+                        }
+                        else
+                        {
+                            // Tell them to either guess lower or higher than the previous guess
+                            Console.WriteLine("Nope, {0} than that.", guessNum < theNumber ? "higher" : "lower");
+                        }
+                    }
+                }
+            } 
+            while (keepGoing);                           
         }
     }
 }
