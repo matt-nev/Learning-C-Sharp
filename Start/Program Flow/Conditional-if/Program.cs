82c4a0b4a61e0673504e11dbe817fc7003768c7b
Matt Nev <mattnev@microsoft.com>
2021-09-19 12:07:35 -0500

    Add overview and program flow finished code

--- Start/Program Flow/Conditional-if/Program.cs
+++ Start/Program Flow/Conditional-if/Program.cs
@@ -6,10 +6,21 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 51;
 
             // TODO: if-else 
-
+            if (theVal == 50)
+            {
+                Console.WriteLine("theVal is 50");
+            }
+            else if (theVal >= 51 && theVal <= 60) 
+            {
+                Console.WriteLine("theVal is between 51 and 60");
+            }
+            else
+            {
+                Console.WriteLine("theVal is something else");
+            }
 
             // -----------------------
             // TODO: Using the ternary operator ?:
@@ -25,7 +36,7 @@ namespace Conditionals
             */
 
             // TODO: can be replaced by a ternary operator ?:
-
+            Console.WriteLine(theVal < 50 ? "theVal is small" : "theVal is large");
         }
     }
 }
